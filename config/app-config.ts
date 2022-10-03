0436d0f83beb53656f188d5946b30d6e5ddf8841
Tengku Hafidz <tengkuhafidz@gmail.com>
2022-10-04 00:37:58 +0800

    fix(login): fix base url

--- config/app-config.ts
+++ config/app-config.ts
@@ -1,6 +1,6 @@
 export const clientConfig = {
     app: {
-        baseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL!
     },
     supabase: {
         baseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
